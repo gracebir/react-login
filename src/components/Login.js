8af259e9cc5cf898f1c7196157bf8a3a11af7944
graceDev1 <grace.devolop@gmail.com>
2021-10-12 10:03:55 +0200

    setup project

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import { Grid, Paper } from '@mui/material'
+
+function Login() {
+    return (
+        <Grid>
+            <Paper elevation={10}>
+
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Login
